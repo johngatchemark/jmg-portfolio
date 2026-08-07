6eeee997f018fd3a4e961c33e9bfb5f63a22baaf
johnmarkgatche <johngatchemark@gmail.com>
2026-08-08 00:59:27 +0800

    fix(accomplishments): modified the top level padding and margins to make them more consistent with the rest of the page

--- src/features/accomplishments/index.tsx
+++ src/features/accomplishments/index.tsx
@@ -63,9 +63,9 @@ const achievementsData: Achievement[] = [
 function AccomplishmentsPage() {
   return (
     <Container>
-      <div className="flex flex-col max-w-120 lg:max-w-5xl xl:w-auto w-full px-6 xs:px-10 lg:px-20 gap-10 py-8">
+      <div className="flex flex-col justify-between align-center gap-10 md:gap-15 max-w-120 lg:max-w-6xl w-full min-w-0 max-w-[calc(100%-5rem)] lg:w-full mx-10 lg:mx-0 lg:px-20 py-8">
         {/* Section 1: Achievements Log */}
-        <section className="flex flex-col gap-4 text-left w-full">
+        <section className="flex flex-col gap-4 text-left w-full min-w-0">
           <p className="text-left text-[12px]! font-mono text-jm-accent tracking-widest">
             &gt; sys.accomplishments()
           </p>
